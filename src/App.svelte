cdc10f2b19bc9c3260b8a3d71bedf3615833e0a2
Sweet-salt <79562182+Sweet-salt@users.noreply.github.com>
2022-01-13 17:22:56 +0900

    Add files via upload

---
+++ src/App.svelte
@@ -0,0 +1,19 @@
+<script>
+    let name = 'hello svelte'
+</script>
+
+<h1>{name}</h1>
+<button class="btn btn-primary">
+    test
+</button>
+
+<style lang="scss">
+
+$color: red;
+h1 {
+    color: $color;
+}
+.h{
+    color: red;
+}
+</style>
