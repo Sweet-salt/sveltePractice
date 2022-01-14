a8926e7129ceaf1c50085bdb3f8578d834c92884
Sweet-salt <79562182+Sweet-salt@users.noreply.github.com>
2022-01-14 13:30:17 +0900

    feat: input 창 만들기

--- src/App.svelte
+++ src/App.svelte
@@ -1,19 +1,8 @@
 <script>
-    let name = 'hello svelte'
+    import CreateTodo from './components/CreateTodo.svelte';
+
 </script>
 
-<h1>{name}</h1>
-<button class="btn btn-primary">
-    test
-</button>
-
-<style lang="scss">
-
-$color: red;
-h1 {
-    color: $color;
-}
-.h{
-    color: red;
-}
-</style>
+<div class="container">
+    <CreateTodo />
+</div>
---
+++ src/components/CreateTodo.svelte
@@ -0,0 +1,26 @@
+<script>
+    let title = ''
+
+    function createTodo() {
+        
+    }
+</script>
+
+<div class="create-todo">
+    <input 
+        type="text" 
+        bind:value={title} 
+        class="form-control"
+        on:keyup={ e => {
+            if(e.key === 'Enter'){
+                createTodo()
+            }
+        }}
+    />
+        <button 
+            class="btn btn-primary"
+            on:click={createTodo}
+        >
+            Create Todo!
+        </button>
+</div>
